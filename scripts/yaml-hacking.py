8c8092c36ab1e46de6c3140654006514716cc51c
ahgraber <ahgraber@ninerealmlabs.com>
2024-04-19 09:15:03 -0400

    feat: yaml hacking to standardize ks

--- scripts/yaml-hacking.py
+++ scripts/yaml-hacking.py
@@ -1,10 +1,25 @@
 # %%
 from pathlib import Path
 from subprocess import CalledProcessError, check_output
+import sys
 
 from ruamel.yaml import YAML
+from ruamel.yaml.representer import RoundTripRepresenter
 
 # import yaml  # use ruamel for comment preservation
+
+
+# %%
+# # Custom representer to handle anchor references
+# def anchor_rep(dumper, data):
+#     """Represent all strings with '*' prefix as yaml anchor aliases."""
+#     if isinstance(data, str) and data.startswith("*"):
+#         return dumper.represent_scalar("tag:yaml.org,2002:str", data, style=None)
+#     return dumper.represent_scalar("tag:yaml.org,2002:str", data)
+
+
+# # Register the custom representer
+# RoundTripRepresenter.add_representer(str, anchor_rep)
 
 # %%
 # Initialize the YAML object with round-trip capabilities
@@ -14,11 +29,12 @@ yaml = YAML(typ="rt")
 yaml.explicit_start = True
 yaml.indent(mapping=2, sequence=4, offset=2)
 
+
 # %%
 try:
-    repo = check_output("git rev-parse --show-toplevel", shell=True)
-except CalledProcessError:
-    raise IOError("Current working directory is not a git repository")
+    repo = check_output(["git", "rev-parse", "--show-toplevel"])  # NOQA: S603, S607
+except CalledProcessError as e:
+    raise IOError("Current working directory is not a git repository") from e
 else:
     repo = Path(repo.decode("utf-8").strip()).resolve()
 
@@ -30,9 +46,12 @@ for hr_file in hrs:
     with open(hr_file, "r") as file:
         hr = yaml.load(file)
 
-        # replace content
-        hr["metadata"].pop("namespace", None)  # remove field
+        # remove field
+        hr["metadata"].pop("namespace", None)
 
+        # update to standardize
+        hr["spec"].update({"interval": "30m"})
+        # replace content
         hr["spec"]["install"] = {
             "remediation": {
                 "retries": 3,
@@ -45,7 +64,8 @@ for hr_file in hrs:
                 "retries": 3,
             },
         }
-        hr["spec"].pop("uninstall", None)  # remove field
+        # remove field
+        hr["spec"].pop("uninstall", None)
 
     with open(hr_file, "w") as file:
         yaml.dump(hr, file)
@@ -55,14 +75,31 @@ kss = repo.glob("**/ks.yaml")
 
 # %%
 for ks_file in kss:
-    for i, subdoc in enumerate(yaml.load_all(Path(ks_file))):
-        # replace content
+    for i, subdoc in enumerate(yaml.load_all(ks_file)):
+        # add 'targetNamespace', update so will see in git diffs
         if "targetNamespace" not in subdoc["spec"]:
             subdoc["spec"]["targetNamespace"] = ""
 
+        # ensure commonMetadata is set
         subdoc["spec"]["commonMetadata"] = {
-            "labels": {"app.kubernetes.io/name": "*app"}
+            "labels": {
+                # "app.kubernetes.io/name": "*app",
+                "app.kubernetes.io/name": subdoc["metadata"]["name"],  # pointing to another scalar value adds an alias
+            }
         }
+
+        # standardize
+        subdoc["spec"].update(
+            {
+                "sourceRef": {
+                    "kind": "GitRepository",
+                    "name": "home-kubernetes",
+                },
+                "interval": "30m",
+                "retryInterval": "1m",
+                "timeout": "5m",
+            }
+        )
 
         if i == 0:
             with open(ks_file, "w") as file:
