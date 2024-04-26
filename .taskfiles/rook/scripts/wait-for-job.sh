0d6f51a1407a5e23e16425d84788052c780a3d86
ahgraber <ahgraber@ninerealmlabs.com>
2024-04-26 16:08:43 -0400

    feat: clean up tasks

--- .taskfiles/rook/scripts/wait-for-job.sh
+++ .taskfiles/rook/scripts/wait-for-job.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
 
-JOB=$1
+JOB_NAME=$1
 NAMESPACE="${2:-default}"
 
-[[ -z "${JOB}" ]] && echo "Job name not specified" && exit 1
+[[ -z "${JOB_NAME}" ]] && echo "Job name not specified" && exit 1
+
 while true; do
-    STATUS="$(kubectl -n "${NAMESPACE}" get pod -l job-name="${JOB}" -o jsonpath='{.items[*].status.phase}')"
+    STATUS="$(kubectl -n "${NAMESPACE}" get pod -l job-name="${JOB_NAME}" -o jsonpath='{.items[*].status.phase}')"
     if [ "${STATUS}" == "Pending" ]; then
         break
     fi
